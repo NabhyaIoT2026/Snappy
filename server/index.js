84d05d73fef9ba1fab9afa26f1c293f6908a25f7
Nabhya Sharma <136943868+NabhyaIoT2026@users.noreply.github.com>
2024-09-14 12:59:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ const messageRoute = require("./routes/messagesRoute");
 const app = express();
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:[https://snappy-livid.vercel.app/],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
